a702babfab222d471c253b02438d291f416905ed
tongyangwang <tongyangwang@tongyangdembp.dhcp.wustl.edu>
2024-02-21 14:13:45 -0600

    !!!

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,13 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle(0.5, 0.3, 0.5, 0.3);
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledCircle(0.25, 0.3, 0.05);
+		StdDraw.filledCircle(0.5, 0.3, 0.05);
+		StdDraw.filledCircle(0.75, 0.3, 0.05);
+		
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,6 +1,7 @@
 package studio4;
 
 import java.io.File;
+import java.awt.Color;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
@@ -20,5 +21,67 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
-	}
+		String shape = in.next();
+		if (shape.equals("ellipse")) {
+			int red = in.nextInt();
+			int green = in.nextInt();
+			int blue = in.nextInt();
+			Color col = new Color(red,green,blue);
+			StdDraw.setPenColor(col);
+			boolean filledOrNot = in.nextBoolean();
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double semiMajorAxis = in.nextDouble();
+			double semiMinorAxis = in.nextDouble();
+			if (filledOrNot) {
+				StdDraw.filledEllipse(x, y, semiMajorAxis, semiMinorAxis);
+			}
+			else {
+				StdDraw.ellipse(x, y, semiMajorAxis, semiMinorAxis);
+			}
+		}
+		else if (shape.equals("rectangle")){
+			int red = in.nextInt();
+			int green = in.nextInt();
+			int blue = in.nextInt();
+			Color col = new Color(red,green,blue);
+			StdDraw.setPenColor(col);
+			boolean filledOrNot = in.nextBoolean();
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeigh = in.nextDouble();
+			if (filledOrNot) {
+				StdDraw.filledRectangle(x, y, halfWidth, halfHeigh);
+			}
+			else {
+				StdDraw.rectangle(x, y, halfWidth, halfHeigh);
+			}
+		}
+		else {
+			int red = in.nextInt();
+			int green = in.nextInt();
+			int blue = in.nextInt();
+			Color col = new Color(red,green,blue);
+			StdDraw.setPenColor(col);
+			boolean filledOrNot = in.nextBoolean();
+			double x1= in.nextDouble();
+			double x2= in.nextDouble();
+			double x3= in.nextDouble();
+			
+			double y1 = in.nextDouble();
+			double y2= in.nextDouble();
+			double y3= in.nextDouble();
+			
+			double[] xArray = {x1,x2,x3};
+			double[] yArray = {y1,y2,y3};
+			
+			if (filledOrNot) {
+				StdDraw.filledPolygon(xArray, yArray);
+			}
+			else {
+				StdDraw.polygon(xArray, yArray);
+			}
+		}
+}
 }
